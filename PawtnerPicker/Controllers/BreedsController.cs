1139411f8c7e22c967129e54703302fa22e34888
Konrad Kisiała <konradkisiala@gmail.com>
2023-06-14 21:09:51 +0200

    Add a very basic dog breed selection form

---
+++ PawtnerPicker/Controllers/BreedsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PawtnerPicker.Models.ViewModels;
+
+namespace PawtnerPicker.Controllers;
+
+public class BreedsController : Controller
+{
+    [HttpGet]
+    public IActionResult Pick()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Pick(PickBreedViewModel pickBreedViewModel)
+    {
+        return RedirectToAction("Index", "Home");
+    }
+}
---
+++ PawtnerPicker/Models/ViewModels/PickBreedViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace PawtnerPicker.Models.ViewModels;
+
+public class PickBreedViewModel
+{
+    public int Popularity { get; set; }
+    public float MinHeight { get; set; }
+    public float MaxHeight { get; set; }
+    public float MinWeight { get; set; }
+    public float MaxWeight { get; set; }
+    public int MinExpectancy { get; set; }
+    public int MaxExpectancy { get; set; }
+    public float GroomingFrequency { get; set; }
+    public float Shedding { get; set; }
+    public float EnergyLevel { get; set; }
+    public float Trainability { get; set; }
+    public float Demeanor { get; set; }
+}
