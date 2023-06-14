820e788d3d082fd85042e7d3f036a3c5a3930748
Konrad Kisiała <konradkisiala@gmail.com>
2023-06-14 15:31:44 +0200

    Initial commit

---
+++ PawtnerPicker/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PawtnerPicker.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
