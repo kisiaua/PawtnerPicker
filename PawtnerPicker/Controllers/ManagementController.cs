1542b68c497f136ac95e771cfbae6a9172fe4527
Konrad Kisiała <konradkisiala@gmail.com>
2023-06-17 10:45:30 +0200

    Add management view

---
+++ PawtnerPicker/Controllers/ManagementController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PawtnerPicker.Controllers;
+
+public class ManagementController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
