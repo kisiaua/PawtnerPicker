ff7bf4d80c995a6e2b6b0a6546b8f6331d74083b
Konrad Kisiała <konradkisiala@gmail.com>
2023-07-04 15:18:57 +0200

    Prevent recommendation on an empty database

--- PawtnerPicker/Controllers/BreedsController.cs
+++ PawtnerPicker/Controllers/BreedsController.cs
@@ -7,9 +7,9 @@ using PawtnerPicker.Models.ViewModels;
 using PawtnerPicker.Services;
 
 namespace PawtnerPicker.Controllers;
+
 public class BreedsController : Controller
 {
-
     private readonly DataContext _dataContext;
     private readonly IPickBreedService _pickBreedService;
 
@@ -30,23 +30,28 @@ public class BreedsController : Controller
     {
         if (!ModelState.IsValid) return View(pickBreedViewModel);
         var recommendations = await _pickBreedService.GetRecommendations(pickBreedViewModel);
-        return View("Recommendations", recommendations);
+        if (recommendations.Count > 0)
+        {
+            return View("Recommendations", recommendations);
+        }
 
+        TempData["NoRecommendations"] = "Database is empty or no recommendations found.";
+        return RedirectToAction("Pick");
     }
-    
+
     [HttpGet]
     public async Task<IActionResult> DisplayBreeds()
     {
         var breeds = await _dataContext.Breeds.ToListAsync();
         return View(breeds);
     }
-    
+
     [HttpGet]
     public async Task<IActionResult> Details(int id)
     {
         var breed = await GetBreedById.GetBreedDetails(_dataContext, id);
         var urlImage = await FetchImage.GetImageUrl(breed.BreedName);
-        
+
         var breedDetailsViewModel = new BreedDetailsViewModel
         {
             Breed = breed,
@@ -54,7 +59,7 @@ public class BreedsController : Controller
         };
         return View(breedDetailsViewModel);
     }
-    
+
     [HttpGet]
     [Authorize(Policy = "RequireAdministratorRole")]
     public async Task<IActionResult> Edit(int id)
@@ -63,21 +68,19 @@ public class BreedsController : Controller
         return View(breed);
     }
 
-    
+
     [HttpPost]
     [Authorize(Policy = "RequireAdministratorRole")]
     public async Task<IActionResult> Edit(Breed updateBreed)
     {
-            var breed = await GetBreedById.GetBreedDetails(_dataContext, updateBreed.Id);
-            UpdateBreedProps.Update(breed, updateBreed);
-            if (!ModelState.IsValid) return View(breed);
-            await _dataContext.SaveChangesAsync();
-            return RedirectToAction("DisplayBreeds");
-
-
+        var breed = await GetBreedById.GetBreedDetails(_dataContext, updateBreed.Id);
+        UpdateBreedProps.Update(breed, updateBreed);
+        if (!ModelState.IsValid) return View(breed);
+        await _dataContext.SaveChangesAsync();
+        return RedirectToAction("DisplayBreeds");
     }
 
-    
+
     [HttpPost]
     [Authorize(Policy = "RequireAdministratorRole")]
     public async Task<IActionResult> Remove(int id)
